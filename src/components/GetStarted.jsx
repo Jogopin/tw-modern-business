ac3372ece83e01f12c511a47eba8973bb9dc1892
jogopin <jonn.g.pineda@gmail.com>
2023-03-28 19:11:24 +0200

    fix minor bug in GetStarted

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -7,13 +7,13 @@ const GetStarted = () => {
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
           <p className='font-poppins font-medium text-[18px] leading-[23px] mr-2'>
-            <spam className="text-gradient">Get</spam>
+            <span className="text-gradient">Get</span>
           </p>
             <img  src={arrowUp} alt="arrow" className="w-[23px] h-[23px] object-contain" />
 
         </div>
         <p className='font-poppins font-medium text-[18px] leading-[23px]'>
-            <spam className="text-gradient">Started</spam>
+            <span className="text-gradient">Started</span>
            
           </p>
       </div>
