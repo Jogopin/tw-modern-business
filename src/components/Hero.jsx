06d5a9185a450cfbad984a83a9f7f51a31836887
jogopin <jonn.g.pineda@gmail.com>
2023-03-28 17:33:12 +0200

    Add GetStarted button in movil view

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import styles from '../style'
-import {discount,robot } from '../assets'
-import GetStarted from './GetStarted'
+import React from "react";
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
 const Hero = () => {
   return (
     <section
@@ -37,14 +37,24 @@ const Hero = () => {
           annual fees.
         </p>
       </div>
-      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`} >
-      <img src={robot} alt="billing" className='w-[100%] h-[100%] relative z-[5]'/>
-      <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
-      <div className='absolute z-[1] w-[80%] h-[80%] white__gradient rounder-full bottom-40 '/>
-      <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient'/>
+      <div
+        className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+      >
+        <img
+          src={robot}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5]"
+        />
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] white__gradient rounder-full bottom-40 " />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+      </div>
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+
       </div>
     </section>
   );
-}
+};
 
-export default Hero
+export default Hero;
